2e85d3964fdcf0b36a950859886a8c9107df0bd6
khil4uk <100033778+khil4uk@users.noreply.github.com>
2022-08-01 17:53:10 +0300

    Update urls.py

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.urls import path
+from calculator.views import recipes_view
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('<recipe>/', recipes_view),
 ]
