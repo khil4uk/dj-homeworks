1d86edc1ee41783027eca8d69805b1760a477728
khil4uk <100033778+khil4uk@users.noreply.github.com>
2022-08-24 09:13:13 +0300

    final_commit

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 
-from .models import Student, Teacher
+from .models import Student, Teacher, StudentPosition
 
+
+class ClassroomPositioninline(admin.TabularInline):
+    model = StudentPosition
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ['group']
+    # list_editable = ['group']
+    list_per_page = 10
+    search_fields = ['name']
+    filter_horizontal = ['teacher']
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'subject']
