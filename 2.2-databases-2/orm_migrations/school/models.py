cf3249a6fd508c6762846c788414e917ef3bdad7
khil4uk <100033778+khil4uk@users.noreply.github.com>
2022-08-24 09:14:28 +0300

    final_commit

--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -2,25 +2,30 @@ from django.db import models
 
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=30, verbose_name='Имя')
-    subject = models.CharField(max_length=10, verbose_name='Предмет')
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
 
     class Meta:
         verbose_name = 'Учитель'
         verbose_name_plural = 'Учителя'
 
     def __str__(self):
-        return self.name
+        return f'{self.name}   {self.subject}'
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    group = models.CharField(max_length=10, verbose_name='Класс')
+    name = models.CharField(max_length=100, default='Александр Александров', verbose_name='Имя')
+    group = models.CharField(max_length=100, default='4-А', verbose_name='Класс')
+    teacher = models.ManyToManyField(Teacher, related_name='students')
 
     class Meta:
         verbose_name = 'Ученик'
         verbose_name_plural = 'Ученики'
 
     def __str__(self):
-        return self.name
+        return f'{self.name}'
+
+
+class StudentPosition(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='position')
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='position')
